1e0272d25aba64ce4a507a0963738c42291e3543
Timur Mazitov <timurmazitov000@gmail.com>
2026-05-27 16:11:22 +0400

    add dev containers with Python 3.10

--- py-0/ex06/ft_filter.py
+++ py-0/ex06/ft_filter.py
@@ -1,8 +1,8 @@
 
 
 def ft_filter(function_to_apply, iterable):
-    """Return an iterator yielding those items of iterable for which function(item)
-    is true. If function is None, return the items that are true."""
+    """Return an iterator yielding those items of iterable for which function
+    (item) is true. If function is None, return the items that are true."""
     if function_to_apply is None:
         return (x for x in iterable if x)
     return (x for x in iterable if function_to_apply(x))
